541d17e169ffb3df54e276db18bfa2a9df02c1cd
Yash Sehgal <yashsehgal.work@gmail.com>
2024-02-24 07:55:31 +0530

    Add better UX to weather widget

--- src/components/weather-card.tsx
+++ src/components/weather-card.tsx
@@ -1,7 +1,23 @@
+import { useState } from 'react';
 import './styles/weather-card.css';
+import { Pencil1Icon, Cross2Icon } from '@radix-ui/react-icons'
+import { motion } from 'framer-motion';
 
 export default function WeatherCard() {
-  return <div className="weather-card">
-    
+  const [hasMouseOver, setHasMouseOver] = useState<boolean>(false);
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+
+  return <div
+    className="weather-card"
+    onMouseEnter={() => setHasMouseOver(true)}
+    onMouseLeave={() => setHasMouseOver(false)}>
+    {hasMouseOver && <motion.button
+      className={!isEditing ? 'edit-location' : 'edit-location edit-location-cancel'}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      onClick={() => setIsEditing(!isEditing)}
+    >
+      {!isEditing ? <Pencil1Icon /> : <Cross2Icon />}
+    </motion.button>}
   </div>
 }
