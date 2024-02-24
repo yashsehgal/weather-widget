cfc09583840f1a215becfe5b72c9d215cad9b1db
Yash Sehgal <yashsehgal.work@gmail.com>
2024-02-24 08:13:56 +0530

    Add UI flow for editing location

--- src/components/weather-card.tsx
+++ src/components/weather-card.tsx
@@ -1,23 +1,40 @@
 import { useState } from 'react';
 import './styles/weather-card.css';
-import { Pencil1Icon, Cross2Icon } from '@radix-ui/react-icons'
+import { Pencil1Icon } from '@radix-ui/react-icons'
 import { motion } from 'framer-motion';
+import { useDisclosure } from '@mantine/hooks';
+import { Modal, Button, SimpleGrid } from '@mantine/core';
+import { Select } from '@mantine/core';
+import { LOCATIONS } from '../common/constants';
 
 export default function WeatherCard() {
   const [hasMouseOver, setHasMouseOver] = useState<boolean>(false);
-  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [opened, { open, close }] = useDisclosure(false);
 
-  return <div
-    className="weather-card"
-    onMouseEnter={() => setHasMouseOver(true)}
-    onMouseLeave={() => setHasMouseOver(false)}>
-    {hasMouseOver && <motion.button
-      className={!isEditing ? 'edit-location' : 'edit-location edit-location-cancel'}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      onClick={() => setIsEditing(!isEditing)}
-    >
-      {!isEditing ? <Pencil1Icon /> : <Cross2Icon />}
-    </motion.button>}
-  </div>
+  return <>
+    <div
+      className="weather-card"
+      onMouseEnter={() => setHasMouseOver(true)}
+      onMouseLeave={() => setHasMouseOver(false)}>
+      {hasMouseOver && <motion.button
+        className={'edit-location'}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        onClick={open}
+      >
+        {<Pencil1Icon />}
+      </motion.button>}
+    </div>
+    <Modal opened={opened} onClose={close} title="Manage Location">
+      <SimpleGrid>
+        <Select
+          label="Location for Weather"
+          placeholder="Select a location"
+          data={LOCATIONS}
+          searchable
+        />
+        <Button onClick={close}>Save location</Button>
+      </SimpleGrid>
+    </Modal>
+  </>
 }
