4e7becc2ff53ffa84b265c383b686cb700a3e52e
Yash Sehgal <yashsehgal.work@gmail.com>
2024-02-24 07:00:33 +0530

    Add weather fetch script

---
+++ src/common/env.ts
@@ -0,0 +1,2 @@
+export const WEATHER_URL = "http://api.weatherapi.com/v1/current.json";
+export const WEATHER_API_KEY = import.meta.env.VITE_WEATHER_API_KEY as string;
---
+++ src/types/global.d.ts
@@ -0,0 +1,10 @@
+declare type WeatherType = {
+  location: string;
+  humidity: number;
+  cloud: number;
+  windDirection: string;
+  condition: {
+    text: string;
+    icon: string;
+  };
+}
