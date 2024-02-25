d76dc05991e69ef6e91a468b6f2528d733f7a071
Yash Sehgal <yashsehgal.work@gmail.com>
2024-02-25 14:49:26 +0530

    empty weatherData render issue resolved

--- src/components/weather-card.tsx
+++ src/components/weather-card.tsx
@@ -38,65 +38,69 @@ export default function WeatherCard() {
     }
   };
 
-  return (
-    <>
-      <div
-        className={`weather-card ${weatherData.cloud > 10 && 'weather-cloud-bg'}`}
-        onMouseEnter={() => setHasMouseOver(true)}
-        onMouseLeave={() => setHasMouseOver(false)}
-        style={{
-          backgroundColor: weatherData.humidity > 20 ? '#d97706' : '#2563eb',
-        }}>
-        {weatherData && (
-          <div className="location-weather-details-wrapper">
-            <div className="weather-icon-wind-direction-wrapper">
-              <img
-                src={weatherData.condition.icon}
-                alt={'weather-icon'}
-                className="weather-icon"
-              />
-              <span className="wind-direction">
-                <img src={WindIcon} className="wind-icon" />
-                {weatherData.windDirection}
-              </span>
+  if (weatherData) {
+    return (
+      <>
+        <div
+          className={`weather-card ${weatherData.cloud > 10 && 'weather-cloud-bg'}`}
+          onMouseEnter={() => setHasMouseOver(true)}
+          onMouseLeave={() => setHasMouseOver(false)}
+          style={{
+            backgroundColor: weatherData.humidity > 20 ? '#d97706' : '#2563eb',
+          }}>
+          {weatherData && (
+            <div className="location-weather-details-wrapper">
+              <div className="weather-icon-wind-direction-wrapper">
+                <img
+                  src={weatherData.condition.icon}
+                  alt={'weather-icon'}
+                  className="weather-icon"
+                />
+                <span className="wind-direction">
+                  <img src={WindIcon} className="wind-icon" />
+                  {weatherData.windDirection}
+                </span>
+              </div>
+              <p className="location">{weatherData.location}</p>
+              <div className="weather-stats-wrapper">
+                <span className="humidity-stats">
+                  {`HUM: ${weatherData.humidity}%`}
+                </span>
+                <span className="cloud-stats">
+                  <img src={CloudIcon} className="cloud-icon" />
+                  {weatherData.cloud}%
+                </span>
+              </div>
             </div>
-            <p className="location">{weatherData.location}</p>
-            <div className="weather-stats-wrapper">
-              <span className="humidity-stats">
-                {`HUM: ${weatherData.humidity}%`}
-              </span>
-              <span className="cloud-stats">
-                <img src={CloudIcon} className="cloud-icon" />
-                {weatherData.cloud}%
-              </span>
-            </div>
-          </div>
-        )}
-        {hasMouseOver && (
-          <motion.button
-            className={'edit-location'}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            onClick={open}>
-            {<Pencil1Icon />}
-          </motion.button>
-        )}
-      </div>
-      <Modal opened={opened} onClose={close} title="Manage Location">
-        <SimpleGrid>
-          <Select
-            onChange={(location) =>
-              location && handleLocationChange(location as string)
-            }
-            label="Location for Weather"
-            placeholder="Select a location"
-            data={LOCATIONS}
-            defaultValue={weatherData.location}
-            searchable
-          />
-          <Button onClick={close}>Save location</Button>
-        </SimpleGrid>
-      </Modal>
-    </>
-  );
+          )}
+          {hasMouseOver && (
+            <motion.button
+              className={'edit-location'}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              onClick={open}>
+              {<Pencil1Icon />}
+            </motion.button>
+          )}
+        </div>
+        <Modal opened={opened} onClose={close} title="Manage Location">
+          <SimpleGrid>
+            <Select
+              onChange={(location) =>
+                location && handleLocationChange(location as string)
+              }
+              label="Location for Weather"
+              placeholder="Select a location"
+              data={LOCATIONS}
+              defaultValue={weatherData.location}
+              searchable
+            />
+            <Button onClick={close}>Save location</Button>
+          </SimpleGrid>
+        </Modal>
+      </>
+    );
+  } else {
+    return <>Loading...</>
+  }
 }
